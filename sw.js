aaf2b7990d703d3f8770faa8397ce2c5b6e4853b
thamyriscardoso <thamy_cardosoo@hotmail.com>
2018-04-03 15:31:19 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,25 @@
-var CACHE_NAME = 'static-v1';
- self.addEventListener('install', function (event) {
+self.addEventListener('install', function(event) {
+  var offlinePage = new Request('offline.html');
   event.waitUntil(
-   caches.open(CACHE_NAME).then(function (cache) {
-   return cache.addAll([
-   '/',
-   'index.html',
-    'css/estilo.css',
-    '/app.js',
-    'manifest.js',
-    '/vendor.js',
-   ]);
-  })
- )
+  fetch(offlinePage).then(function(response) {
+    return caches.open('pwabuilder-offline').then(function(cache) {
+      console.log('[PWA Builder] Cached offline page during Install'+ response.url);
+      return cache.put(offlinePage, response);
+    });
+  }));
 });
-
-var CACHE_NAME = 'static-v1';
-  self.addEventListener('activate', function activator(event) {
-  event.waitUntil(
-    caches.keys().then(function (keys) {
-      return Promise.all(keys
-        .filter(function (key) {
-        return key.indexOf(CACHE_NAME) !== 0;
-        })
-        .map(function (key) {
-            return caches.delete(key);
-          })                 
-      );
-    })
-  );
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function(error) {
+        console.error( '[PWA Builder] Network request Failed. Serving offline page ' + error );
+        return caches.open('pwabuilder-offline').then(function(cache) {
+          return cache.match('offline.html');
+      });
+    }));
 });
- 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-    caches.match(event.request).then(function (cachedResponse) {
-      return cachedResponse || fetch(event.request);
-   })
- );
-});
- 
-caches.open('example-cache').then(function(cache) {
-  cache.add('/example-file.html');
-});
-caches.open('example-cache').then(function(cache) {
-  cache.matchAll('/images/').then(function(response) {
-     response.forEach(function(element, index, array) {
-       cache.delete(element);
-     });
+self.addEventListener('refreshOffline', function(response) {
+  return caches.open('pwabuilder-offline').then(function(cache) {
+    console.log('[PWA Builder] Offline page updated from refreshOffline event: '+ response.url);
+    return cache.put(offlinePage, response);
   });
-})
+});
