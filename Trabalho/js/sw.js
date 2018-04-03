ac7a0f7f8c074df97a32865c3d66dc09444bae46
thamyriscardoso <thamy_cardosoo@hotmail.com>
2018-04-03 14:26:27 -0300

    Update sw.js

--- Trabalho/js/sw.js
+++ Trabalho/js/sw.js
@@ -4,10 +4,9 @@ self.addEventListener('install', function (event) {
    caches.open(CACHE_NAME).then(function (cache) {
    return cache.addAll([
    '/',
-   '/index.html',
-   '/styles.css',
+   'index.html',
+   'css/estilo.css',
    '/app.js',
-   '/manifest.js',
-   '/vendor.js',
+   'manifest.js'
  ]);
 })
