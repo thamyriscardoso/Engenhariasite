17e1ec283f58af03499cda6f27c65e70a310f956
thamyriscardoso <thamy_cardosoo@hotmail.com>
2018-04-03 15:18:18 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var CACHE_NAME = 'static-v1';
-self.addEventListener('install', function (event) {
- event.waitUntil(
+ self.addEventListener('install', function (event) {
+  event.waitUntil(
    caches.open(CACHE_NAME).then(function (cache) {
    return cache.addAll([
    '/',
