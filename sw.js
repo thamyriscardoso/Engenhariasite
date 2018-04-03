277fcb38b98671d1feafe25b466ff20197a2ad17
thamyriscardoso <thamy_cardosoo@hotmail.com>
2018-04-03 15:16:05 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,7 +10,10 @@ self.addEventListener('install', function (event) {
     '/manifest.js',
     '/vendor.js',
    ]);
-})
+  })
+ )
+});
+
 var CACHE_NAME = 'static-v1';
   self.addEventListener('activate', function activator(event) {
   event.waitUntil(
