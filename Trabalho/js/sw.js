28a74034cd41c97d08be5718f0f451539742cce7
thamyriscardoso <thamy_cardosoo@hotmail.com>
2018-04-03 14:31:50 -0300

    Update sw.js

--- Trabalho/js/sw.js
+++ Trabalho/js/sw.js
@@ -10,3 +10,27 @@ self.addEventListener('install', function (event) {
    'manifest.js'
  ]);
 })
+
+var CACHE_NAME = 'static-v1';
+  self.addEventListener('activate', function activator(event) {
+   event.waitUntil(
+    caches.keys().then(function (keys) {
+     return Promise.all(keys
+      .filter(function (key) {
+       return key.indexOf(CACHE_NAME) !== 0;
+      })
+     .map(function (key) {
+        return caches.delete(key);
+      })                 
+    );
+  })
+ );
+});
+ 
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
+   })
+ );
+});
