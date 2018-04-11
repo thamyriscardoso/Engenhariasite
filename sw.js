1028cbec7b538a21b79cd3b74e5dd6737689a91f
thamyriscardoso <thamy_cardosoo@hotmail.com>
2018-04-11 10:01:03 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,68 @@
-self.addEventListener('install', function(event) {
-  var offlinePage = new Request('offline.html');
-  event.waitUntil(
-  fetch(offlinePage).then(function(response) {
-    return caches.open('pwabuilder-offline').then(function(cache) {
-      console.log('[PWA Builder] Cached offline page during Install'+ response.url);
-      return cache.put(offlinePage, response);
-    });
-  }));
+const CACHE_NAME = 'cantinho-dos-engordes';
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll([
+                '/Engenhariasite/',
+                '/Engenhariasite/index.html',
+                '/Engenhariasite/acompanhamentos.html',
+                '/Engenhariasite/bebidas.html',
+                '/Engenhariasite/cadastro.html',
+                '/Engenhariasite/cadastrocerto.html',
+                '/Engenhariasite/cadastrocerto1.html',
+                '/Engenhariasite/cadastrocerto2.html',
+                '/Engenhariasite/cadastrocerto3.html',
+                '/Engenhariasite/carrinho.html',
+                '/Engenhariasite/contatos.html',
+                '/Engenhariasite/fotos.html',
+                '/Engenhariasite/lanches.html',
+                '/Engenhariasite/offline.html',
+                '/Engenhariasite/pizza.html',
+                '/Engenhariasite/produtos.html',
+                '/Engenhariasite/promocoes.html',
+                '/Engenhariasite/manifest.json',
+                '/Engenhariasite/css/estilo.css',
+                '/Engenhariasite/css/bootstrap-responsive.css',
+                '/Engenhariasite/css/bootstrap-responsive.min.css',
+                '/Engenhariasite/css/bootstrap.min.css',
+                '/Engenhariasite/css/jumbotron-narrow.css',
+                '/Engenhariasite/css/signin.css',
+                '/Engenhariasite/img/burguer.png',
+                '/Engenhariasite/img/carne.png',
+                '/Engenhariasite/img/fritas.png',
+                '/Engenhariasite/img/lanche.jpg',
+                '/Engenhariasite/img/pizza.jpg',
+                '/Engenhariasite/img/torre.png',
+                '/Engenhariasite/js/bootstrap.js',
+                '/Engenhariasite/js/bootstrap.min.js',
+                '/Engenhariasite/js/jquery.min.js',
+                '/Engenhariasite/js/main.js',
+                '/Engenhariasite/js/npm.js',
+            ])
+        })
+    )
 });
 
-//If any fetch fails, it will show the offline page.
-//Maybe this should be limited to HTML documents?
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function(error) {
-        console.error( '[PWA Builder] Network request Failed. Serving offline page ' + error );
-        return caches.open('pwabuilder-offline').then(function(cache) {
-          return cache.match('offline.html');
-      });
-    }));
+self.addEventListener('activate', function activator(event) {
+    event.waitUntil(
+        caches.keys().then(function (keys) {
+            return Promise.all(keys
+                .filter(function (key) {
+                    return key.indexOf(CACHE_NAME) !== 0;
+                })
+                .map(function (key) {
+                    return caches.delete(key);
+                })
+            );
+        })
+    );
 });
 
-//This is a event that can be fired from your page to tell the SW to update the offline page
-self.addEventListener('refreshOffline', function(response) {
-  return caches.open('pwabuilder-offline').then(function(cache) {
-    console.log('[PWA Builder] Offline page updated from refreshOffline event: '+ response.url);
-    return cache.put(offlinePage, response);
-  });
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (cachedResponse) {
+            return cachedResponse || fetch(event.request);
+        })
+    );
 });
