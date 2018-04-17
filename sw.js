116a6411a7c102e715809aac9dcebdfc7548aa0e
thamyriscardoso <thamy_cardosoo@hotmail.com>
2018-04-17 15:26:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,68 +1,71 @@
-
 self.addEventListener('install', function (event) {
+    var indexPage = new Request('index.html');
     event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            return cache.addAll([
-                '/Engenhariasite/',
-                '/Engenhariasite/index.html',
-                '/Engenhariasite/acompanhamentos.html',
-                '/Engenhariasite/bebidas.html',
-                '/Engenhariasite/cadastro.html',
-                '/Engenhariasite/cadastrocerto.html',
-                '/Engenhariasite/cadastrocerto1.html',
-                '/Engenhariasite/cadastrocerto2.html',
-                '/Engenhariasite/cadastrocerto3.html',
-                '/Engenhariasite/carrinho.html',
-                '/Engenhariasite/contatos.html',
-                '/Engenhariasite/fotos.html',
-                '/Engenhariasite/lanches.html',
-                '/Engenhariasite/offline.html',
-                '/Engenhariasite/pizza.html',
-                '/Engenhariasite/produtos.html',
-                '/Engenhariasite/promocoes.html',
-                '/Engenhariasite/manifest.json',
-                '/Engenhariasite/css/estilo.css',
-                '/Engenhariasite/css/bootstrap-responsive.css',
-                '/Engenhariasite/css/bootstrap-responsive.min.css',
-                '/Engenhariasite/css/bootstrap.min.css',
-                '/Engenhariasite/css/jumbotron-narrow.css',
-                '/Engenhariasite/css/signin.css',
-                '/Engenhariasite/img/burguer.png',
-                '/Engenhariasite/img/carne.png',
-                '/Engenhariasite/img/fritas.png',
-                '/Engenhariasite/img/lanche.jpg',
-                '/Engenhariasite/img/pizza.jpg',
-                '/Engenhariasite/img/torre.png',
-                '/Engenhariasite/js/bootstrap.js',
-                '/Engenhariasite/js/bootstrap.min.js',
-                '/Engenhariasite/js/jquery.min.js',
-                '/Engenhariasite/js/main.js',
-                '/Engenhariasite/js/npm.js',
-                '/Engenhariasite/sw.js'
-            ])
-        })
-    )
-});
-self.addEventListener('activate', function activator(event) {
-    event.waitUntil(
-        caches.keys().then(function (keys) {
-            return Promise.all(keys
-                .filter(function (key) {
-                    return key.indexOf(CACHE_NAME) !== 0;
-                })
-                .map(function (key) {
-                    return caches.delete(key);
-                })
-            );
-        })
+       fetch(indexPage).then(function (response) {
+            caches.open('pwabuilder-offline').then(function (cache) {
+                console.log('[PWA Builder] Cached index page during Install' + response.url);
+                return cache.addAll([
+                    '/Engenhariasite/',
+                    '/Engenhariasite/index.html',
+                    '/Engenhariasite/acompanhamentos.html',
+                    '/Engenhariasite/bebidas.html',
+                    '/Engenhariasite/cadastro.html',
+                    '/Engenhariasite/cadastrocerto.html',
+                    '/Engenhariasite/cadastrocerto1.html',
+                    '/Engenhariasite/cadastrocerto2.html',
+                    '/Engenhariasite/cadastrocerto3.html',
+                    '/Engenhariasite/carrinho.html',
+                    '/Engenhariasite/contatos.html',
+                    '/Engenhariasite/fotos.html',
+                    '/Engenhariasite/lanches.html',
+                    '/Engenhariasite/offline.html',
+                    '/Engenhariasite/pizza.html',
+                    '/Engenhariasite/produtos.html',
+                    '/Engenhariasite/promocoes.html',
+                    '/Engenhariasite/manifest.json',
+                    '/Engenhariasite/css/estilo.css',
+                    '/Engenhariasite/css/bootstrap-responsive.css',
+                    '/Engenhariasite/css/bootstrap-responsive.min.css',
+                    '/Engenhariasite/css/bootstrap.min.css',
+                    '/Engenhariasite/css/jumbotron-narrow.css',
+                    '/Engenhariasite/css/signin.css',
+                    '/Engenhariasite/img/burguer.png',
+                    '/Engenhariasite/img/carne.png',
+                    '/Engenhariasite/img/fritas.png',
+                    '/Engenhariasite/img/lanche.jpg',
+                    '/Engenhariasite/img/pizza.jpg',
+                    '/Engenhariasite/img/torre.png',
+                    '/Engenhariasite/js/bootstrap.js',
+                    '/Engenhariasite/js/bootstrap.min.js',
+                    '/Engenhariasite/js/jquery.min.js',
+                    '/Engenhariasite/js/main.js',
+                    '/Engenhariasite/js/npm.js',
+                    '/Engenhariasite/sw.js'
+            ]);
+        });
     );
 });
+self.addEventListener('fetch', function(event) {
+  var updateCache = function(request){
+    return caches.open('pwabuilder-offline').then(function (cache) {
+      return fetch(request).then(function (response) {
+        console.log('[PWA Builder] add page to offline'+response.url)
+        return cache.put(request, response);
+      });
+    });
+  };
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function (cachedResponse) {
-            return cachedResponse || fetch(event.request);
-        })
-    );
-});
+  event.waitUntil(updateCache(event.request));
 
+  event.respondWith(
+    fetch(event.request).catch(function(error) {
+      console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
+      return caches.open('pwabuilder-offline').then(function (cache) {
+        return cache.match(event.request).then(function (matching) {
+          var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
+          return report
+        });
+      });
+    })
+  );
+})
